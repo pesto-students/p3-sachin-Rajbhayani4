756cd9822bbb76272272724b361dd64dcccb1010
raj <rajbhayani4@gmail.com>
2022-07-03 09:51:26 +0530

    add changes

--- Week-3/Assignment3.1/index.js
+++ Week-3/Assignment3.1/index.js
@@ -1,25 +1,18 @@
-const memoize = (fun) => {
+const memoize = (functions) => {
     let cache = {}
     return (...args) => {
-        console.log(args, "args")
         let n = args[0];
-        console.log(n, "nn")
         if (n in cache) {
-            console.log("Work IN")
             return cache[n]
         } else {
-            console.log("Work First Time")
-            let result = args.reduce((acc, curr) => fun(acc, curr), 0);
-            console.log(result, "result")
+            let result = args.reduce((acc, curr) => functions(acc, curr), 0);
             cache[n] = result;
             return result;
         }
     }
 }
 
-const add = (a, b) => {
-    return a + b
-}
+const add = (a, b) => a + b
 
 const memoizeAdd = memoize(add)
 console.log(memoizeAdd(100, 100), "memoizeAdd");
